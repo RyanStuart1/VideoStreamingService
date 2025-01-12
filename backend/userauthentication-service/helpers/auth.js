9a493814d1b6e2cdf290e555adec2e0f99c666b5
Ryan <ryanstuart911@gmail.com>
2025-01-12 21:28:47 +0000

    bcryptjs

--- backend/userauthentication-service/helpers/auth.js
+++ backend/userauthentication-service/helpers/auth.js
@@ -1,26 +1,26 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs'); // Use bcryptjs instead of bcrypt
 
 const hashPassword = (password) => {
     return new Promise((resolve, reject) => {
         bcrypt.genSalt(12, (err, salt) => {
-            if(err) {
-                reject(err)
+            if (err) {
+                reject(err);
             }
             bcrypt.hash(password, salt, (err, hash) => {
-                if(err) {
-                    reject(err)
+                if (err) {
+                    reject(err);
                 }
-                resolve(hash) 
-            })
-        })
-    })
-}
+                resolve(hash);
+            });
+        });
+    });
+};
 
 const comparePassword = (password, hashed) => {
-    return bcrypt.compare(password, hashed)
-}
+    return bcrypt.compare(password, hashed); // Works the same way as bcrypt
+};
 
 module.exports = {
     hashPassword,
-    comparePassword
-}
+    comparePassword,
+};
