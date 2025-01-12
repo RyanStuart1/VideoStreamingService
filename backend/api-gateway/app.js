97c05364c9e8f10c9fbf0fd3e0a366070646bb27
Ryan <ryanstuart911@gmail.com>
2025-01-12 23:39:13 +0000

    fix express rate limit

--- backend/api-gateway/app.js
+++ backend/api-gateway/app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
-const rateLimit = require('express-rate-limit');
-
 const app = express();
 
 // Middleware
