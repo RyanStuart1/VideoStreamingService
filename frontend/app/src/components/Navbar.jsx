1867b3b472b8dcee70b411c7d5727bc0cd28a518
Ryan <ryanstuart911@gmail.com>
2025-01-13 16:44:01 +0000

    css update

--- frontend/app/src/components/Navbar.jsx
+++ frontend/app/src/components/Navbar.jsx
@@ -1,11 +1,61 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav>
-      <Link to="/" style={{ marginRight: '15px' }}>Home</Link>
-      <Link to="/register" style={{ marginRight: '15px' }}>Register</Link>
-      <Link to="/login">Login</Link>
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "10px 20px",
+        backgroundColor: "black",
+        borderBottom: "1px solid red",
+      }}
+    >
+      {/* Home button on the left */}
+      <div>
+        <Link
+          to="/"
+          style={{
+            color: "white",
+            textDecoration: "none",
+            padding: "5px 10px",
+            border: "1px solid red",
+            borderRadius: "5px",
+          }}
+        >
+          Home
+        </Link>
+      </div>
+
+      {/* Register and Login buttons on the right */}
+      <div>
+        <Link
+          to="/register"
+          style={{
+            color: "white",
+            textDecoration: "none",
+            padding: "5px 10px",
+            border: "1px solid red",
+            borderRadius: "5px",
+            marginRight: "10px",
+          }}
+        >
+          Register
+        </Link>
+        <Link
+          to="/login"
+          style={{
+            color: "white",
+            textDecoration: "none",
+            padding: "5px 10px",
+            border: "1px solid red",
+            borderRadius: "5px",
+          }}
+        >
+          Login
+        </Link>
+      </div>
     </nav>
   );
 }
